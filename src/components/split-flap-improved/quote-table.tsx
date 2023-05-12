9afabfe6133804fa3a1f78a0a4a255a70f03e589
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-05-11 19:36:48 -0600

    Refactor quoteTable into setInterval, prevent too-long rows.

--- src/components/split-flap-improved/quote-table.tsx
+++ src/components/split-flap-improved/quote-table.tsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from 'react';
 import CharacterCyclerImproved from './character-cycler-improved';
+import { isMobile } from 'react-device-detect';
 
+const letters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+const punctuation = ["'",'-','.',",", '!', '?'];
+
+const characters = [' ', ...letters, ...punctuation];
 
 function QuoteTable({quote}){
 
-  // temp for desktop
-  const rowLength = 20;
+  const rowLength = isMobile ? 10 : 20;
+  const maxRows = isMobile ? 10 : 5;
 
-    // splits quote into individual rows and returns an array of those rows
+  function initialState(rowLength: number, maxRows: number) {
+    let initialArray = [];
+    for (let i = 0; i < maxRows; i++) {
+      initialArray.push(Array(rowLength).fill(' '));
+    }
+    return initialArray;
+  }
+
+  const [currentState, setCurrentState] = useState(initialState(rowLength, maxRows));
+
+  const [count, setCount] = useState(0);
+
+  // splits quote into individual rows and returns an array of those rows
+  // TODO: reject quotes when they contain a single word over 10 characters
+  // or when adding the end space makes the row over 10 characters
   function quoteInRows(quote: string, rowLength: number){
     if (!quote) return [];
     let quoteRowsArray = [];
@@ -20,14 +39,15 @@ function QuoteTable({quote}){
             line = trialLine;
             words.shift();
           } else {
-            quoteRowsArray.push(line.trim());
+            quoteRowsArray.push(line.trim().toUpperCase());
             line = '';
             break;
           }
         }
         // line that was too short to trigger push
         if (line.length) {
-          quoteRowsArray.push(line.trim());
+          // TODO: convert to arrays here - needed for deep equality
+          quoteRowsArray.push(line.trim().toUpperCase());
         }
       }
       return quoteRowsArray;
@@ -35,42 +55,56 @@ function QuoteTable({quote}){
 
   const quoteForTable = quoteInRows(quote, rowLength);
 
-  // const maxRows = rowLength === 20 ? 5 : 10;
-  // temp for desktop
-  const maxRows = 5;
-
   function tableRows(quote: Array<string>){
     if (!quote) return [];
-    while (quote.length < 5) {
-      quote.push(''.padEnd(rowLength - 2));
-    }
     return (
       <>
        {quote.map(
-        (item: string, index: number) => 
-          <tr key={`${index}-${item.charAt(0)}`} className="border border-custom-blue">
-            {tableData(item.padEnd(rowLength - 1, ' '))}
+        (item: Array<string>, index: number) => 
+          <tr className="border border-custom-blue">
+            {tableData(item)}
           </tr>
        )}
       </>
     );
   }
 
-  function tableData(item: Array<string>) {
-    const itemIndividualChars = [...item.toUpperCase()];
-    return itemIndividualChars.map((char, index) => 
-      <td key={`${index}-${char}`} className="bg-slate-300 border border-custom-blue text-center td-width text-4xl font-split-flap text-custom-blue">
-        <CharacterCyclerImproved stopCharacter={char} />
-      </td>);
+  function advanceState(currentState: Array<string>, finalState: Array<string>){
+    for (let row = 0; row < finalState.length; row++){
+      for(let col = 0; col < finalState[row].length; col++) {
+        if (currentState[row][col] !== finalState[row][col]) {
+           const index = characters.indexOf(currentState[row][col]);
+           currentState[row][col] = characters[(index + 1) % characters.length];
+        }
+      }
+    }
+    return currentState;
   }
 
-  const tableRowElements = tableRows(quoteForTable);
+  useEffect(() => {
+    setCurrentState(initialState(rowLength, maxRows));
+    const timeout = setInterval(() => {
+      setCurrentState((prevState) => advanceState(prevState, quoteForTable))
+      // temp workaround due to React shallow equality/invisible state updates in inner arrays
+      setCount((prevCount) => prevCount + 1);
+    }, 125);
+    return () => {
+      clearInterval(timeout);
+    }
+  }, [quote]);
+
+  function tableData(item: Array<string>) {
+    return item.map((char, index) => 
+      <td className="bg-slate-300 border border-custom-blue text-center td-width text-4xl font-split-flap text-custom-blue">
+        {char}
+      </td>);
+  }
 
   return (
     <>
       <table className="border-collapse">
         <tbody>
-          {tableRowElements}
+          {tableRows(currentState)}
         </tbody>
       </table>
     </>
--- src/components/split-flap/quotes.ts
+++ src/components/split-flap/quotes.ts
@@ -1,19 +1,19 @@
 const quotes = [
   'Life is not about finding yourself. Life is about creating yourself.',
   'Success is the sum of small efforts, repeated day-in and day-out.',
-  'Courage is resistance to fear, mastery of fear - not absence of fear.', 
   'You must expect great things of yourself before you can do them.',
-  'Motivation is what gets you started. Habit is what keeps you going.',
-  'Opportunities don\'t happen, you create them.',
-  'Everything you\'ve ever wanted is on the other side of fear.',
   'Never say never, because limits, like fears, are often just an illusion.',
   'The most difficult thing is the decision to act, the rest is merely tenacity.',
-  'The best time to plant a tree was 20 years ago. The next best time is now.',
+  'The best time to plant a tree was twenty years ago. The next best time is now.',
   'Talent hits a target no one else can hit. Genius hits a target no one else can see.',
   'Don\'t judge each day by the harvest you reap, but by the seeds that you plant.',
   'The most important step one can take is not the first one. It\'s the next one.',
   'Believe you can and you\'re halfway there.',
-  'Someday is not a day of the week.'
+  'Someday is not a day of the week.',
+  'If I cannot do great things, I can do small things in a great way.',
+  'Write it on your heart that every day is the best day in the year.',
+  'Never let the fear of striking out keep you from playing the game.',
+  'Doubt ends more dreams than failure ever will.'
  ];
 
 export default quotes;
