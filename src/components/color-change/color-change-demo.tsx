4d924b0cbfd2eff651129f48b9b60d87344910ac
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-05-16 11:36:28 -0600

    Add todo to adjust message timing.

--- src/components/color-change/color-change-demo.tsx
+++ src/components/color-change/color-change-demo.tsx
@@ -3,14 +3,15 @@ interface ColorChangeDemoProps {
   displayNoMatchMessage: boolean;
 }
 
+// TODO: adjust mismatch message timing
 function ColorChangeDemo({ progressMessage, displayNoMatchMessage }: ColorChangeDemoProps) {
   return (
     <>
       <div className="ml-6">
         <p className="">{progressMessage}</p>
-        { displayNoMatchMessage &&
+        {/*{ displayNoMatchMessage &&
           <p className="">That wasn't a matching color, try again!</p>
-        }
+        }*/}
       </div>
     </>
   );
